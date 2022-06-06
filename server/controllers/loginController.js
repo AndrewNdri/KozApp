1baac8db7d46ebeaa81daa74f87aa569e733a5a1
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-06 12:29:13 +0000

    "Added user login"

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,3 +1,20 @@
-module.exports.loginController = (req, res) =>{
-    res.send('Its is login');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const {loginValidation} = require('../utils/validation');
+
+module.exports.loginController = async(req, res) =>{
+    //Data validation
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //checking if the email exists
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(404).send('User not found');
+
+    //checking if the password is correct
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid password');
+
+    console.log(user);
+    res.status(200).send("User logged in");
 };
