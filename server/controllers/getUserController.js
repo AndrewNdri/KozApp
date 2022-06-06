baec00a1c76e78076b0d3af8b947ab02d71508bc
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-06 17:58:10 +0000

    "get user controller updated"

--- server/controllers/getUserController.js
+++ server/controllers/getUserController.js
@@ -3,7 +3,8 @@ const User = require('../models/User');
 module.exports.getUserController = async (req, res) =>{
     try{
         const user = await User.findById(req.params.id);
-        res.status(200).json(user);
+        const {password, updatedAt,...other} = user._doc;
+        res.status(200).json(other);
     }catch(err){
         res.status(500).send(err);
     }
