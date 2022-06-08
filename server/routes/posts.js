c1784a934fc54b6b24c925ec5889b6d2fe488021
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-08 15:25:42 +0000

    "update post added"

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {createPostController} = require('../controllers/createPostController');
+const {updatePostController} = require('../controllers/updatePostController');
 
 router.post('/', createPostController);
+router.put('/:id', updatePostController);
 
 module.exports = router;
