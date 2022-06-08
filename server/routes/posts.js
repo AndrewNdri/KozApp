10b3aca0aa06e1ee5b20c7e3f89c0e6d9f2f1009
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-08 15:53:07 +0000

    "like dislike post added"

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {createPostController} = require('../controllers/createPostController');
 const {updatePostController} = require('../controllers/updatePostController');
 const {deletePostController} = require('../controllers/deletePostController');
+const {likePostController} = require('../controllers/likePostController');
 
 router.post('/', createPostController);
 router.put('/:id', updatePostController);
 router.delete('/:id', deletePostController);
+router.put('/:id/like', likePostController);
 
 module.exports = router;
