7a33e3771359bc1add0ceb3e2b5de00b4f17e7f3
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-10 15:42:22 +0000

    "rightbar updated"

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,11 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+export default function Rightbar({profile}) {
+
+  const HomeRightbar = ()=>{
+    return(
+      <>
         <div className="birthdayContainer">
           <img src="" alt="" className="birthdayImg" />
           <span className="birthdayText">
@@ -17,6 +18,56 @@ export default function Rightbar() {
         <ul className="rightbarFriendsList">
           {Users.map(u=> <Online key={u.id} user={u}/>)}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = ()=>{
+    return(
+      <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Takoradi</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Abidjan</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src="assets/lion.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingNam">Vince Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/lion.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingNam">Vince Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/lion.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingNam">Vince Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/lion.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingNam">Vince Carter</span>
+          </div>
+          
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightbar/>
       </div>
     </div>
   );
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,32 @@
 import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <Topbar/>
+        <div className="profile">
+            <Sidebar/>
+            <div className="profileRight">
+                <div className="profileRightTop">
+                    <div className="profileCover">
+                        <img src="assets/lion.jpeg" alt="" className="profileCoverImg" />
+                        <img src="assets/person/cheetah.jpeg" alt="" className="profileUserImg" />
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">Andrew N'Dri</h4>
+                        <span className="profileInfoDesc">This is it</span>
+                    </div>
+                </div>
+                <div className="profileRightBottom">
+                    <Feed/>
+                    <Rightbar profile/>
+                </div>
+            </div>
+        </div>
+    </>
   );
 }
