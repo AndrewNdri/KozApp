20808a1ccb90e87e99e14b81da66b5e3a3e23b03
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-10 12:15:11 +0000

    "rightbar added"

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,25 @@ import "./rightbar.css";
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Justin Raines</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendsList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/cheetah.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Aaron Spears</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
