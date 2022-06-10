9765ecf23a080eaa425b2cf6c6563e9429f304d0
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-10 02:57:57 +0000

    "updating feed and share files"

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,23 @@
+import "./share.css";
+import {PermMedia} from "@mui/icons-material"
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+          <div className="shareTop">
+              <img className="shareProfileImg" src="/assets/person/cheetah.jpeg" alt="" />
+              <input placeholder="What's in your mind?" className="shareInput" />
+          </div>
+          <hr className="shareHr"/>
+          <div className="shareBottom">
+              <div className="shareOptions">
+                  <div className="shareOption">
+                      <PermMedia className="shareIcon"/>
+                      <span className="shareOptionText">Photo or Video</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+}
