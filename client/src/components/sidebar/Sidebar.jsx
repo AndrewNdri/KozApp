60db45e4b41b15cccc0788f85ed78206ecccb2fd
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-10 01:53:16 +0000

    "sidebar added"

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,33 @@
-import "./sidebar.css"
+import "./sidebar.css";
+import { RssFeed, Chat, PlayCircle } from "@mui/icons-material";
 
 export default function Sidebar(){
     return (
         <div className="sidebar">
-            sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon"/>
+                        <span className="sidebarListItemTest">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItemTest">Chat</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircle className="sidebarIcon"/>
+                        <span className="sidebarListItemTest">Videos</span>
+                    </li>
+                </ul>
+                {/* <button className="sidebarButton">Show</button> */}
+                <hr className="sidebarHr" />
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/cheetah.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Andrew N'Dri</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
