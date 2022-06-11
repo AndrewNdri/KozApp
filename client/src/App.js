35d0cb0cdeddb940ef2f70c2ed3e88928091c28a
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-11 15:03:14 +0000

    "Added routes to App.js file"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-//import Home from "./pages/home/Home";
- import Profile from "./pages/profile/Profile";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register"; 
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return <Profile/>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/profile" element={<Profile/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
