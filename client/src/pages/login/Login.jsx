7f6d0275b7b2657db09f7cae00c18ff125aaf881
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-14 11:45:30 +0000

    "password input modified and context files created"

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
             <div className="loginBottom">
                 <div className="loginBox">
                     <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
+                    <input placeholder="Password" type="password" className="loginInput" />
                     <button className="loginButton">Log In</button>
                     <span className="loginForgot">Forgot Password?</span>
                     <button className="loginRegisterButton">Create a new account</button>
