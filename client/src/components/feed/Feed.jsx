816f35cb0af10af518c43adc1ec8de5e050f69c8
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-16 12:32:49 +0000

    "feed, post and share files updated"

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,22 +1,24 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 //import {Posts} from "../../dummyData";
 import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Feed({username}) {
   const [posts, setPosts] = useState([]);
+  const {user} = useContext(AuthContext);
 
   useEffect(()=>{
     const fetchPosts = async()=>{
       const res = username 
       ? await axios.get(`/posts/profile/${username}`)
-      : await axios.get("posts/timeline/62a012cd0487ba62d528b3c6");
+      : await axios.get(`posts/timeline/${user._id}`);
       setPosts(res.data);
     };
     fetchPosts();
-  },[username]);
+  },[username, user._id]);
   return (
     <div className="feed">
       <div className="feedWrapper">
