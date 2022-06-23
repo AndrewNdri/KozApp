5fc078efa3fb0ba8d770c77ad626071165546418
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-23 14:11:22 +0000

    "create new message route added"

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const {createNewMessageController} = require("../controllers/createNewMessageController");
+
+//add
+router.post("/", createNewMessageController);
+
+//get
 
 module.exports = router;
