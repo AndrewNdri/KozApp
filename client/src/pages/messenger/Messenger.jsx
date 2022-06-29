fd59f979d1195cd93a0b67e08e614450caef53c6
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-29 17:02:48 +0000

    "Added chat functionality"

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -14,14 +14,32 @@ export default function Messenger() {
     const [currentChat, setCurrentChat] = useState(null);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
-    const [socket, setSocket] = useState(null);
-
+    const [arrivalMessage, setArrivalMessage] = useState(null);
+    const socket = useRef();
     const user = useSelector((state) => state.userReducer);
     const scrollRef = useRef();
 
     useEffect(()=>{
-        setSocket(io("ws://localhost:8900"));
-    },[]);
+        socket.current = io("ws://localhost:8900");
+        socket.current.on("getMessage", data =>{
+            setArrivalMessage({
+                sender: data.senderId,
+                text: data.text,
+                createdAt: Date.now()
+            });
+        })
+    }, []);
+
+    useEffect(()=>{
+        arrivalMessage && currentChat?.members.includes(arrivalMessage.sender) && setMessages(prev=>[...prev, arrivalMessage])
+    },[arrivalMessage, currentChat]);
+
+    useEffect(()=>{
+       socket.current.emit("addUser", user._id);
+       socket.current.on("getUsers", users=>{
+        console.log(users)
+       });
+    },[user]);
 
     useEffect(() => {
       const getConversations = async ()=>{
@@ -54,6 +72,13 @@ export default function Messenger() {
             text:newMessage,
             conversationId: currentChat._id
         };
+
+        const receiverId = currentChat.members.find(member=>member !== user._id)
+        socket.current.emit("sendMessage",{
+            senderId: user._id,
+            receiverId,
+            text: newMessage
+        })
 
         try{
             const res = await axios.post("/messages", message);
